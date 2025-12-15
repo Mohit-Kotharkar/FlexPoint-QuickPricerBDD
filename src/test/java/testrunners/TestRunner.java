0c8d1bf49015873a7c4c15ac9c6d03ac408fd6a6
afshansji <jahafbl101@gmail.com>
2025-12-15 13:52:01 +0530

    first commit

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -7,7 +7,11 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/java/features/LLPAAdjustments.feature",
         // features = "src/test/java/features/",
         glue = {"stepdefinitions"},
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        plugin = {
+                "pretty", "html:target/cucumber-reports.html",
+        // write failed scenario locations to a rerun file for easy re-run tooling
+                "rerun:target/rerun.txt",
+        },
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
