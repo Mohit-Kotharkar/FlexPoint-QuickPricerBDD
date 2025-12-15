0632d40f2b3f9a535b9bce9dcfc4244a31e60be3
afshansji <jahafbl101@gmail.com>
2025-12-15 14:06:33 +0530

    second commit

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -14,5 +14,12 @@ import io.cucumber.testng.CucumberOptions;
         },
         monochrome = true
 )
+import org.testng.annotations.DataProvider;
+
 public class TestRunner extends AbstractTestNGCucumberTests {
+        @Override
+        @DataProvider(parallel = true)
+        public Object[][] scenarios() {
+                return super.scenarios();
+        }
 }
